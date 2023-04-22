1b478bc058ca9fad2b965bd484856dcd335c42b4
--add <ikorlit@gmail.com>
2023-04-22 02:40:00 -0700

    our main.h

---
+++ pod.h
@@ -0,0 +1,14 @@
+#ifndef _POD_H_
+#define _POD_H_
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+ 
+#endif
