efeaa23e2d577c8d3f41df8c1aa12f60858735fb
--add <ikorlit@gmail.com>
2023-04-27 05:13:18 -0700

    updated

--- prompt.c
+++ prompt.c
@@ -1,80 +1,20 @@
 #include "pod.h"
-#include "string.h"
 
 /**
- * Main - Entry point for the program.
- *
- * This function reads user input, performs calculations, and outputs results.
- * It takes no arguments and returns an integer value indicating exit status.
- *
- * Return: -1
- */
+* main - Counts numbers 1-20 and writes them to standard output
+*
+* Return: 0 if successful
+*/
 int main(void)
 {
-	char *caller = "#shelly& ";
-	char *lineptr;
-	size_t n = 0;
-	ssize_t read_newchar;
+	char buffer[3];
+	int n, i;
 
-
-	while (1)
+	for (i = 1; i <= 20; i++)
 	{
-		write(STDOUT_FILENO, caller, strlen(caller));
-		read_newchar = getline(&lineptr, &n, stdin);
-
-		if (read_newchar == -1)
-		{
-			perror("Escape shell----\n");
-			free(lineptr);
-			return (-1);
-		}
-
-		/*Pemove the new line character from the end of input*/
-		lineptr[strcspn(lineptr, "\n")] = '\0';
-
-		/*Split the input into commands and arguements*/
-		char *command;
-		char *arg;
-		char *argv[3];
-
-		argv[0] = command;
-		argv[1] = arg;
-		argv[2] = NULL;
-		
-
-		/*Execute the command with execve*/
-		if (command)
-		{
-			pid_t pid = fork();
-			if (pid == -1)
-			{
-				perror("fork");
-				exit(EXIT_FAILURE);
-			}
-			else if(pid == 0)
-			{
-				/* Child proces*/
-                char *argv[] = {command, arg, NULL};
-                char *envp[] = {NULL};
-                execve(command, argv, envp);
-
-                /* If execve returns, it means an error occurred*/
-                perror(command);
-                exit(EXIT_FAILURE);
-            }
-            else
-            {
-                /* Parent process*/
-                int status;
-                if (waitpid(pid, &status, 0) == -1)
-                {
-                    perror("waitpid");
-                    exit(EXIT_FAILURE);
-				}
-			}
-		}
+		n = sprintf(buffer, "%d\n", i);
+		write(STDOUT_FILENO, buffer, n);
 	}
-	free (lineptr);
+
 return (0);
 }
-
